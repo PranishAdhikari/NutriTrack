0729e180763f44993d3d81f838c89bf1c1b2c1e7
jaxonconiglio <142911688+jaxonconiglio@users.noreply.github.com>
2025-08-04 16:02:32 -0400

    Update HomePage.xaml.cs

--- NutriTrack/Views/HomePage.xaml.cs
+++ NutriTrack/Views/HomePage.xaml.cs
@@ -6,4 +6,14 @@ public partial class HomePage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+	private async void OnTrackMealClicked(object sender, EventArgs e)
+	{
+		await DisplayAlert("Track Meal", "Navigate to meal tracking page.", "OK");
+	}
+
+	private async void OnViewStatsClicked(object sender, EventArgs e)
+	{
+		await DisplayAlert("View Stats", "Navigate to nutrition stats page", "OK");
+	}
 }
